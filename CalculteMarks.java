4786cd9ab81a68672a13170ed3341091ef8a0968
krRupeshChauhan <117341601+krRupeshChauhan@users.noreply.github.com>
2022-11-03 13:16:56 +0530

    basicprogramingcode

---
+++ CalculteMarks.java
@@ -0,0 +1,49 @@
+/*Calculate mark
+ *
+ */
+
+package basicprogram;
+import java.util.Scanner;
+public class CalculteMarks {
+	public static void main(String[]args) {
+		//declare the variables
+		int math,hindi,english,science,IT,sum,avg,percentage;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("no of math");
+		math=sc.nextInt();
+		System.out.println("no of hindi");
+		hindi=sc.nextInt();
+		System.out.println("no of english");
+		english=sc.nextInt();
+		System.out.println("no of Science");
+		science=sc.nextInt();
+		System.out.println("no of IT");
+		IT=sc.nextInt();
+		sum=(math+hindi+english+science+IT);
+		//calculate the total marks
+		System.out.println("Total marks"+sum);
+		avg=sum/5;
+		System.out.println("avg narks"+avg);
+		if (sum>=300)
+	
+		{
+			System.out.println("pass");
+			
+		}
+		else {	System.out.println("fail");
+			
+		}
+	
+		
+		
+				
+		
+	
+		
+		
+		
+		
+		
+	}
+
+}
